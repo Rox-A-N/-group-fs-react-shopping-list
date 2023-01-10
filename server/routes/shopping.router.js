cb5a61cc1674b61026df6a3362a8443072b6d7c8
Roxanne Alvidrez Niederkorn <rox.niederkorn@gmail.com>
2023-01-09 20:56:33 -0600

    Co-authored-by: Klee55 <Klee55@users.noreply.github.com> Co-authored-by: Linssi Colin Ruiz <NotLindskey@users.noreply.github.com> Co-authored-by: CupaJ12 <CupaJ12@users.noreply.github.com>

--- server/routes/shopping.router.js
+++ server/routes/shopping.router.js
@@ -5,7 +5,7 @@ const pool = require('../modules/pool.js');
 // GET
 router.get('/', (req, res) => {
     console.log('in GET request')
-    let queryText = 'SELECT * FROM shoppingList';
+    let queryText = `SELECT * FROM "shoppingList"`;
 
     pool.query(queryText).then((result) =>{
         res.send(result.rows);
@@ -13,19 +13,19 @@ router.get('/', (req, res) => {
         console.log(err);
         res.sendStatus(500);
     })
-})
+});
 // POST
 router.post('/', (req, res) => {
     console.log('in POST request', req.body)
-    let queryText = 'INSERT INTO shoppingList ("name", "quantity", "unit") VALUES ($1, $2, $3);'
+    let queryText = `INSERT INTO "shoppingList" (name, quantity, unit) VALUES ($1, $2, $3);`;    
     pool.query(queryText, [req.body.name, req.body.quantity, req.body.unit] )
-    pool.query(queryText).then((result) =>{
+    .then((result) =>{
         res.sendStatus(200);
     }).catch((err) =>{
         console.log(err);
         res.sendStatus(500)
     })
-})
+});
 // DELETE
 
 // PUT
